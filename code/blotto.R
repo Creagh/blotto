2b49e7e5ee5031c4a39a5f096286780eaf0eb294
Creagh Briercliffe <creagh5@hotmail.com>
2017-11-03 09:22:15 -0700

    add more deterministically chosen placements

--- code/blotto.R
+++ code/blotto.R
@@ -15,13 +15,13 @@ source("tweak_placements.R")
 # IDEA: sample resource placements from a Dirichlet-Multinomial distribution, 
 # where we choose the vector of shape parameters to simulate different strategy regimes
 
-set.seed(2023)
+set.seed(2024)
 
 # Set parameters
 n <- 10 # number of districts
 r <- 100 # number of resources
 num_strat <- 5 # number of different strategies (e.g. front-loaded, uniform, etc.)
-num_place <- 5000 # number of resource placements to simulate for each strategy
+num_place <- 15000 # number of resource placements to simulate for each strategy
 total_num <- num_strat * num_place # total number of simulated placements
 
 # Set the vector of shape parameters to choose different strategy regimes
@@ -64,21 +64,25 @@ x_old <- x
 x <- x_new
 
 # Add some (deterministically) chosen placements
-#x <- rbind(x, c(100, rep(0,9)))
-#x <- rbind(x, c(rep(0,7), 9, 0, 91))
-#x <- rbind(x, c(rep(0,6), 8, 0, 1, 91))
-#x <- rbind(x, c(51, 49, rep(0,8)))
+x <- rbind(x, c(100, rep(0,9)))
+x <- rbind(x, c(rep(0,7), 9, 0, 91))
+x <- rbind(x, c(rep(0,6), 8, 0, 1, 91))
+x <- rbind(x, c(51, 49, rep(0,8)))
 x <- rbind(x, c(37,3,4,5,6,7,8,9,10,11))
-#x <- rbind(x, c(91, rep(1,9)))
-#x <- rbind(x, c(rep(1,9), 91))
+x <- rbind(x, c(91, rep(1,9)))
+x <- rbind(x, c(rep(1,9), 91))
 x <- rbind(x, c(36,13,13,5,6,7,8,1,10,1))
 x <- rbind(x, c(0,0,4,0,11,13,15,17,19,21))
 x <- rbind(x, c(1,1,1,1,11,13,15,17,19,21))
 x <- rbind(x, c(4,0,0,0,11,13,15,17,19,21))
 x <- rbind(x, c(0,3,0,1,11,13,15,17,19,21))
 x <- rbind(x, c(47,7,13,21,2,2,2,2,2,2))
+x <- rbind(x, c(12,3,13,21,6,7,8,9,10,11))
+x <- rbind(x, c(16,11,7,25,6,7,8,9,10,1))
+x <- rbind(x, c(12,11,16,25,1,7,8,9,10,1))
+x <- rbind(x, c(4,11,4,5,31,7,8,9,10,11))
 
-total_num <- total_num + 7
+total_num <- total_num + 17
 (num_matches <- choose(total_num, 2)) # total number of matches that will be played
 
 # Count the number of unique placements sampled
@@ -121,7 +125,7 @@ index3 <- which(avg == min(avg))
 x[index3,]
 hist(votes[index3,])
 
-# Find the top 1% scoring placements
+# Find the top 20% scoring placements
 cutoff <- quantile(avg, prob=0.99)
 top1pct <- which(avg >= cutoff)
 order <- order(avg[top1pct], decreasing=TRUE)
@@ -130,7 +134,4 @@ x[top1pct[order],]
 # a few more summaries...
 ggplot(data.frame(avg=avg), aes(x=avg)) + geom_density()
 
-df <- data.frame(avg=c(avg[1:4000], avg[4001:8000], avg[8001:12000]), strategy=c(rep("uniform", 4000), rep("front-load", 4000),
-																																								 rep("back-load", 4000)))
-ggplot(df, aes(x=avg, fill=strategy)) + geom_density(alpha=0.25) + geom_vline(xintercept=avg[12001:12012])
-avg[12001:12012]
+avg[75001:75013]
